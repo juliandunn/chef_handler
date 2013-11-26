6a1c79ccad6a8df9de57e6216b550f860486a734
Julian C. Dunn <jdunn@opscode.com>
2013-11-25 21:27:01 -0500

    WiP -- add start handlers

--- resources/default.rb
+++ resources/default.rb
@@ -23,12 +23,12 @@ actions :enable, :disable
 attribute :class_name, :kind_of => String, :name_attribute => true
 attribute :source, :default => nil, :kind_of => String
 attribute :arguments, :default => []
-attribute :supports, :kind_of => Hash, :default => { :report => true, :exception => true }
+attribute :supports, :kind_of => Hash, :default => { :start => false, :report => true, :exception => true }
 
 # we have to set default for the supports attribute 
 # in initializer since it is a 'reserved' attribute name
 def initialize(*args)
   super
   @action = :enable
-  @supports = { :report => true, :exception => true }
+  @supports = { :start => false, :report => true, :exception => true }
 end
